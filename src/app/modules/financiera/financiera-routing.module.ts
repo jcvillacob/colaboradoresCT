a5fb0282573cf292f919ef236f20aa164b3e53c8
jcvillacob <jucaviza6@gmail.com>
2025-02-19 15:42:58 -0500

    feature: with boletin and async

--- src/app/modules/financiera/financiera-routing.module.ts
+++ src/app/modules/financiera/financiera-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NovedadesCarteraComponent } from './components/novedades-cartera/novedades-cartera.component';
 import { TerpelComponent } from './components/terpel/terpel.component';
+import { AuthGuard } from 'src/app/core/authentication/auth.guard';
 
 const routes: Routes = [
-  {path: 'novedades-cartera', component: NovedadesCarteraComponent},
-  {path: 'facturacion-terpel', component: TerpelComponent}
+  {path: 'novedades-cartera', component: NovedadesCarteraComponent, canActivate: [AuthGuard], data: { subroles: ['Novedades Cartera']} },
+  {path: 'facturacion-terpel', component: TerpelComponent, canActivate: [AuthGuard], data: { subroles: ['Facturacion Terpel']} }
 ];
 
 @NgModule({
